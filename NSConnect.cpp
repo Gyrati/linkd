ffce386bfc86b0be22fd1aa14b6ee9dff77da86c
Gyrati <mixaa@live.de>
2018-10-10 16:20:31 +0200

    Revert "Update NSConnect.cpp"
    This reverts commit 1156ad6c9991f1f152e21ee8cd73122d081ff416.

--- NSConnect.cpp
+++ NSConnect.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	netStream ns = netStream(NSOT_CONNECT, false);
 	ns.set_connect_data("127.0.0.1", 2232);
+	ns.set_listen_data("127.0.0.1", 222);
 	ns.open();
 	std::string bf = "1";
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -20,7 +21,8 @@ int main()
 		{
 			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
-		pkhlol
+		std::cout << "Answer: " << ns.ns_pool_peers.back()->nsp_queue_payload_next() << std::endl;
+		ns.ns_pool_peers.back()->nsp_queue_payload_pop();
 		//ns.ns_pool_peers.back()->nsp_broadcast("This is a test where Whitespace is not divided when received by server");
 		// In this example, the payload is divided by Whitespace. THIS DOES NOT HAPPEN IF SENT DATA != CIN
 	}
